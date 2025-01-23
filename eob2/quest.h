4066194f57fd49c9dc845ec182468f1ab3c5ecb3
Pavelius <p000000000001@gmail.com>
2025-01-23 13:04:14 +0200

    Before quest refactor.

--- eob2/quest.h
+++ eob2/quest.h
@@ -7,18 +7,29 @@
 
 struct dungeoni;
 
-enum stagen : char {
-	StageActive, StageClosed,
+enum questf : char {
+	QuestActive, QuestPrepared, QuestClosed,
 };
-
 struct quest : nameable {
 	struct leveli : dungeon_site {
 		variants features;
 	};
+	struct historyi {
+		short unsigned	monster;
+		char			stage, value;
+	};
+	typedef adat<historyi, 8> historya;
+	typedef sliceu<dungeoni> dungeona;
 	char		difficult; // From 0 to 5. Add to magic item level.
 	goala		goals;
 	variants	travel, reward;
+	flag8		flags;
 	leveli		sites[8];
+	historya	history;
+	dungeona	dungeon;
+	bool		is(questf v) const { return flags.is(v); }
+	void		remove(questf v) { flags.remove(v); }
+	void		set(questf v) { flags.set(v); }
 };
 extern quest* last_quest;
 extern adat<quest*> party_quests;

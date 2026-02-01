897ad2fee76bb69288f4cf8f5c185b7daefa97c4
Pavelius <p000000000001@gmail.com>
2026-02-01 22:06:48 +0200

    Locale improvement.

--- eob2/main.cpp
+++ eob2/main.cpp
@@ -17,14 +17,12 @@ void util_main();
 int main() {
 	start_random_seed = getcputime();
 	//start_random_seed = 1423089921;
-	current_locale[0] = 'u';
-	current_locale[1] = 'a';
 	srand(start_random_seed);
 	initialize_gui();
 	music_initialize();
 	initialize_translation();
 	initialize_strings();
-	log::readlf(speech_read, "names", "*.txt");
+	log::readlf(speech_read, "locale", "*.str");
 	bsreq::read("rules/Core.txt");
 	avatar_read("rules/Avatars.txt");
 	if(log::errors > 0)
